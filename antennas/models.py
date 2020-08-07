6de9463f299ad14dd7b3e56a80dff2cf6d24b69a
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-08-07 13:51:16 -0700

    add hex plotly dash app and migrations necessary to include all ant stats

--- antennas/models.py
+++ antennas/models.py
@@ -80,16 +80,86 @@ class AprioriStatus(models.Model):
 
 
 class AntennaStatus(models.Model):
+    """
+     Definition of antenna status table (based on SNAP info).
+
+     Very Similar to the Atenna Status table from HERA MC
+
+     Attributes
+     ----------
+     antenna_number : Antenna Instance
+        An instance of the Antenna Class. The antenna number and polarization.
+     time : Datetime Column
+         GPS time of the antenna status data, floored. Part of primary_key.
+     snap_hostname : String Column
+         SNAP hostname.
+     snap_channel_number : Integer Column
+         The SNAP ADC channel number (0-7) to which this antenna is connected.
+     adc_mean : Float Column
+         Mean ADC value, in ADC units.
+     adc_rms : Float Column
+         RMS ADC value, in ADC units.
+     adc_power : Float Column
+         Mean ADC power, in ADC units squared.
+     pam_atten : Integer Column
+         PAM attenuation setting for this antenna, in dB. (Integer)
+     pam_power : Float Column
+         PAM power sensor reading for this antenna, in dBm.
+     pam_voltage : Float Column
+         PAM voltage sensor reading for this antenna, in Volts.
+     pam_current : Float Column
+         PAM current sensor reading for this antenna, in Amps.
+     pam_id : String Column
+         Serial number of this PAM.
+     fem_voltage : Float Column
+         FEM voltage sensor reading for this antenna, in Volts.
+     fem_current : Float Column
+         FEM current sensor reading for this antenna, in Amps.
+     fem_id : String Column
+         Serial number of this FEM.
+     fem_switch : String Column
+         Switch state for this FEM. Options are: {'antenna', 'load', 'noise'}
+     fem_lna_power : Boolean Column
+         Power state of this FEM (True if powered).
+     fem_imu : Array Column
+         IMU-reported theta and phi, in degrees.
+     fem_temp : Float Column
+         EM temperature sensor reading for this antenna in degrees Celsius.
+     fft_overflow : Boolean Column
+         Indicator of an FFT overflow, True if there was an FFT overflow.
+     eq_coeffs : Array Column
+         Digital EQ coefficients for this antenna,
+     adc_hist : Array of Array Column
+         2D array of [[ADC histogram bin centers],[ADC histogram counts]],
+
+    """
+
     antenna = models.ForeignKey(Antenna, on_delete=models.CASCADE)
     time = models.DateTimeField("Status Time")
+
     snap_hostname = models.CharField(max_length=200, blank=True, null=True)
     snap_channel_number = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    adc_mean = models.FloatField(blank=True, null=True)
+    adc_rms = models.FloatField(blank=True, null=True)
+    adc_power = models.FloatField(blank=True, null=True)
+
+    pam_atten = models.IntegerField(blank=True, null=True)
+    pam_power = models.FloatField(blank=True, null=True)
+    pam_voltage = models.FloatField(blank=True, null=True)
+    pam_current = models.FloatField(blank=True, null=True)
+    pam_id = models.CharField(max_length=200, blank=True, null=True)
+
+    fem_voltage = models.FloatField(blank=True, null=True)
+    fem_current = models.FloatField(blank=True, null=True)
     fem_id = models.CharField(max_length=200, blank=True, null=True)
     fem_lna_power = models.BooleanField(blank=True, null=True)
     # fem IMU Theta and Phi
     fem_imu = ArrayField(models.FloatField(), 2, blank=True, null=True)
     fem_temp = models.FloatField(blank=True, null=True)
+
     fft_overflow = models.BooleanField(blank=True, null=True)
+
     eq_coeffs = ArrayField(models.FloatField(), blank=True, null=True)
     # adc histograms first row centers, second row values
     adc_hist = ArrayField(ArrayField(models.FloatField()), blank=True, null=True)
