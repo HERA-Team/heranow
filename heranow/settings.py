5002d0c56ce5ebe9d5d22ccc4dd2bd809f138c07
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2020-09-01 14:34:01 -0700

    use environ to load envirnoment variables

--- heranow/settings.py
+++ heranow/settings.py
@@ -11,34 +11,41 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+for scheme in ["postgres", "psql", "postgresql", "pgsql"]:
+    env.DB_SCHEMES[scheme] = "django.db.backends.postgresql_psycopg2"
+# reading .env file
+environ.Env.read_env(env_file=str(BASE_DIR / ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(BASE_DIR, ".django_key"), "r") as key_file:
+with open(BASE_DIR / env.str("DJANGO_KEY"), "r") as key_file:
     SECRET_KEY = key_file.read()
 
-with open(os.environ.get("GITHUB_APP_KEY_FILE", ""), "r") as keyfile:
+with open( BASE_DIR / env.str("GITHUB_APP_KEY_FILE"), "r") as keyfile:
     GITHUB_APP_KEY = keyfile.read()
-with open(os.environ.get("GITHUB_APP_ID_FILE", ""), "r") as appid_file:
+with open( BASE_DIR /  env.str("GITHUB_APP_ID_FILE"), "r") as appid_file:
     GITHUB_APP_ID = appid_file.read()
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
+INTERNAL_IPS = env.list("DJANGO_INTERNAL_IPS")
+
 DATA_UPLOAD_MAX_MEMORY_SIZE = None
 # Application definition
 
@@ -76,7 +83,7 @@ ROOT_URLCONF = "heranow.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -97,13 +104,7 @@ WSGI_APPLICATION = "heranow.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "heranow_db",
-        "USER": "heranow",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
+    "default": env.db_url("DJANGO_DB")
 }
 
 
@@ -136,13 +137,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
-    os.path.join(os.path.join(BASE_DIR, "dashboard"), "static"),
+    BASE_DIR/ "dashboard" / "static",
     # os.path.join(BASE_DIR, "static"),
 ]
 # List of finder classes that know how to find static files in
@@ -176,8 +177,8 @@ PLOTLY_COMPONENTS = [
 
 
 # CELERY Configuration
-CELERY_BROKER_URL = "redis://localhost:6380"
-CELERY_RESULT_BACKEND = "redis://localhost:6380"
+CELERY_BROKER_URL = env.str("CELERY_REDIS_URL")
+CELERY_RESULT_BACKEND = env.str("CELERY_REDIS_URL") 
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
