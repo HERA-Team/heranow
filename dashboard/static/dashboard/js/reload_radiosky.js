58c2b02010411f524efece530d27a8c4c66e77c1
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-09-01 14:04:46 -0700

    only set reload interval after page has loaded

--- dashboard/static/dashboard/js/reload_radiosky.js
+++ dashboard/static/dashboard/js/reload_radiosky.js
@@ -1,5 +1,6 @@
 function reloadradiosky() {
  var frameHolder=document.getElementById('radiosky');
 frameHolder.src = frameHolder.src;
+  // document.getElementById("radiosky").location.reload(true);
 }
-setInterval(reloadradiosky, 30000);
+window.onload = setInterval(reloadradiosky, 30000);
