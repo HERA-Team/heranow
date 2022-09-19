80d607f2ff256e9683fcd6adb66471bcc2fba851
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2022-09-19 12:16:03 -0700

    update entrypoints to point to correct activate

--- dockerfiles/entrypoints/entrypoint.sh
+++ dockerfiles/entrypoints/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH=/opt/conda/envs/heranow/bin:$PATH
-source activate heranow
+source /opt/conda/envs/heranow/bin/activate heranow
 python manage.py migrate
 python manage.py collectstatic --noinput
 
@@ -15,6 +15,5 @@ fi
 if [ "$DJANGO_DEBUG" -eq  "1" ]; then
     uvicorn heranow.asgi:application --port ${PORT} --reload
 else
-    gunicorn -k uvicorn.workers.UvicornWorker -w 14 --threads 24 heranow.asgi:application -b :${PORT} --timeout 300
-    #--max-requests 100000 --max-requests-jitter 10000
+    gunicorn -k uvicorn.workers.UvicornWorker -w 14 --threads 24 heranow.asgi:application -b :${PORT} --timeout 300 --max-requests 100000 --max-requests-jitter 10000
 fi
--- dockerfiles/entrypoints/entrypoint_celery.sh
+++ dockerfiles/entrypoints/entrypoint_celery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PATH=/opt/conda/envs/heranow/bin:$PATH
-source activate heranow
+source /opt/conda/envs/heranow/binactivate heranow
 
 celery -A heranow worker -l INFO --autoscale=10,3
